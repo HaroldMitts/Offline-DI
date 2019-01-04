af9e69207e9e617494f6a61f24716faee960459f
Harold Mitts <HaroldMitts@users.noreply.github.com>
2019-01-03 15:06:17 -1000

    Update PS-DI.ps1
    Added comments

--- PS-DI.ps1
+++ PS-DI.ps1
@@ -1,4 +1,31 @@
-﻿$SysManufacturer = (Get-WmiObject -Class:Win32_ComputerSystem).Manufacturer
+﻿<# This script will check the WMI to get the PC manufacturer, PC model, and OS architecture
+and then will attempt to find network folder paths named after the values stored in the devices WMI. 
+If matching folders are found, DISM will install drivers to the offline installation. 
+
+This script should be run after Windows installation completes, but while the device is still booted 
+to WinPE. 
+
+Network share where drivers are saved is assumed to be Z:\Drivers
+
+Prerequisite: You must add the PowerShell optional component to WinPE for WinPE to support this script
+OC install order: 
+1. WinPE-WMI.cab
+2. WinPE-WMI_en-us.cab
+3. WinPE-Scripting.cab
+4. WinPE-Scripting_en-us.cab 
+5. WinPE-NetFx.cab
+6. WinPE-NetFx_en-us.cab
+7. WinPE-Scripting_en-us.cab
+8. WinPE-PowerShell.cab
+9. WinPE-PowerShell_en-us.cab
+
+Place your INF drivers into folders named after WMI values. 
+Example:  D:\Drivers\Microsoft Corporation\Virtual Machine\64-bit
+Caution: DISM /Recurse will install all drivers found in the folder. For this reason, only drivers 
+which match the device should be saved to this location. This will help avoid loading unnecessary drivers.
+#>
+
+$SysManufacturer = (Get-WmiObject -Class:Win32_ComputerSystem).Manufacturer
 $SysModel = (Get-WmiObject -Class:Win32_ComputerSystem).Model
 $OSArch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 
